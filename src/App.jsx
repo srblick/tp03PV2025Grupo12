e28ba90431bd111df83f3a10943ee3fd80b15969
Joel Garcia <joel77270@gmail.com>
2025-04-29 19:30:23 -0300

    Implementacion inicial de la logica para la lista de tareas por Joel Garcia

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import TaskInput from './assets/components/TaskInput.jsx'
+import TaskList from './assets/components/TaskList.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([]);
+    console.clear(); 
+     console.log("Estado actual de las tareas :", tasks);
+    const addTask = (text) => {
+        const newTask = {
+            id: Date.now(), // ID unico usando la fecha del sistema
+            texto: text,  // Texto de la tarea
+            completado: false // Estado inicial
+        }
+        setTasks([...tasks, newTask]); // Agrega la nueva tarea al array existente
+    }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const toggleTask = (id) => {
+        // Actualiza el estado de la tarea usando setTasks
+        setTasks(tasks.map(task =>  // Recorre todas las tareas existentes
+            // Si coincide: crea copia de la tarea invirtiendo su estado. Si no coincide: devuelve la tarea sin cambios
+            task.id === id ? { ...task, completado: !task.completado } : task 
+        ))
+    }
+
+    return(
+        <div className="tareas-container">
+            <h1>Lista de tareas</h1>
+            <TaskInput addTask={addTask} />
+            <TaskList tasks={tasks} toggleTask={toggleTask} />
+        </div>
+    )
 }
 
 export default App
